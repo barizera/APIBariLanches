efc2122c08181ad62b6abef3df9fe3256268637d
Felipe Bari <luizfelipebari@gmail.com>
2022-06-27 21:44:03 -0300

    Fix: Types from endpoints

--- src/Users/users.controller.ts
+++ src/Users/users.controller.ts
@@ -2,6 +2,7 @@ import { Body, Controller, Get, Post } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { CreateUserDto } from './dto/create.user.dto';
 import { ApiTags } from '@nestjs/swagger';
+import { User } from './entity/users.entity';
 
 @ApiTags('users')
 @Controller('users')
@@ -9,12 +10,12 @@ export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
   @Get()
-  getAll() {
+  getAll(): User[] {
     return this.usersService.getAll();
   }
 
   @Post()
-  create(@Body() createUserDto: CreateUserDto) {
+  create(@Body() createUserDto: CreateUserDto): User {
     return this.usersService.create(createUserDto);
   }
 }
